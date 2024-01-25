6bc52e64121c5a70974d1c55093ed67472d34108
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2024-01-25 12:06:03 +0000

    think cronjob from ls3

--- extract-db.py
+++ extract-db.py
@@ -3,13 +3,13 @@ import pyarrow.parquet as pq
 import numpy as np
 
 # Charger le DataFrame depuis le fichier Parquet
-df = pq.read_table('chemin_vers_votre_fichier.parquet').to_pandas()
+df = pq.read_table('base_sirene_ape.parquet').to_pandas()
 
 # Convertir la colonne de dates en format datetime si ce n'est pas déjà fait
-df['date_column'] = pd.to_datetime(df['date_column'])
+df['date_modification_dt'] = pd.to_datetime(df['date_modification'])
 
 # Filtrer les lignes à partir du 1er janvier 2023
-df = df[df['date_column'] >= '2023-01-01']
+df = df[df['date_modification_dt'] >= '2023-01-01']
 
 # Extraire n lignes au hasard uniformément
 n = 10  # Remplacez 10 par le nombre de lignes que vous souhaitez extraire
@@ -20,4 +20,4 @@ print(random_rows)
 
 # Sauvegarder le résultat dans un nouveau fichier Parquet
 output_file = 'chemin_vers_output.parquet'
-pq.write_table(pa.Table.from_pandas(random_rows), output_file)
+pq.write_table(pq.Table.from_pandas(random_rows), output_file)
