49d721887fba12ee78c54d82074300c402edb2f1
nrandriamanana <nathan.randriamanana@eleve.ensai.fr>
2024-02-12 17:46:16 +0000

    queue

--- extract-db.sh
+++ extract-db.sh
@@ -1,5 +1,5 @@
 mc cp --recursive s3/projet-ape/extractions/20240212_sirene4.parquet ./
 EXTRACT_DB=$(python extract-db.py "20240212_sirene4.parquet" "2500")
-mc mv $EXTRACT_DB "s3/projet-ape/label-studio/annotation-campaign-2024/NAF2008/data-samples/"
+mc mv $EXTRACT_DB "s3/projet-ape/label-studio/annotation-campaign-2024/NAF2008/data-samples/queue/"
 
-# mc cp --recursive "s3/projet-ape/Label Studio/Annotation APE 2024/NAF 2008/Annotation target/Lot 2/" "s3/projet-ape/Label Studio/Annotation APE 2024/NAF 2008/Annotation target/data/Lot_2/"
+# mc cp --recursive "s3/projet-ape/Label Studio/Annotation APE 2024/NAF 2008/Extract manuelle/Archive annotations/" "s3/projet-ape/label-studio/annotation-campaign-2024/NAF2008/data-samples/archive/"
